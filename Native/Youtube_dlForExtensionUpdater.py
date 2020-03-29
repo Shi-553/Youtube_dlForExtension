9fc6293ee7cf9443d0da9dae2c5abbad80f42bfc
Shi <sibatamaru@gmail.com>
2020-03-30 00:58:51 +0900

    I added it because I forgot "import sys".

--- Native/Youtube_dlForExtensionUpdater.py
+++ Native/Youtube_dlForExtensionUpdater.py
@@ -1,12 +1,12 @@
-﻿import os
+﻿import sys
+import os
 import urllib.request
 
 #ファイル名
 myFilename= os.path.basename(sys.argv[0])
-
 if myFilename == "Youtube_dlForExtensionUpdater.py":
-     url="https://drive.google.com/uc?id=1DNA02s4mn9bvQBSfZqXP6ThpUUB1RIO9"
-     filename="Youtube_dlForExtension.py"
+    url="https://drive.google.com/uc?id=1DNA02s4mn9bvQBSfZqXP6ThpUUB1RIO9"
+    filename="Youtube_dlForExtension.py"
 
 elif myFilename == "Youtube_dlForExtensionUpdater.exe":
     url="https://drive.google.com/uc?id=1MRGbG68I-07P91MJoLOV0onVDikFXlhd"
