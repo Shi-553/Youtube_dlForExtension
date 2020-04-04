66b7b6b7fffad36514c6feb4b38b4ed10496c9da
Shi <sibatamaru@gmail.com>
2020-04-04 23:26:24 +0900

    Miscellaneous updates

---
+++ Native/Youtube_dlForExtensionBuild.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d %~dp0
+
+py -3-64 -m PyInstaller Youtube_dlForExtension.py --onedir  --onefile --clean --noconsole
+py -3-64 -m PyInstaller Youtube_dlForExtensionUpdater.py --onedir  --onefile --clean --noconsole
+pause
---
+++ Native/setup.py
@@ -0,0 +1,14 @@
+﻿from distutils.core import setup
+import py2exe
+
+option = {
+    "bundle_files":2,
+    "compressed": True,
+    "includes": ["tkinter"]
+}
+setup(
+    options = {"py2exe": option},
+    windows = [
+    {"script" : "Youtube_dlForExtension.py"}],
+    zipfile = None 
+    )
