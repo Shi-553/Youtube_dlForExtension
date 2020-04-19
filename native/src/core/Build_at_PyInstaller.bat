2180d0e3d9db986abb5ada5fb81a945d9dd7d02e
Shi <sibatamaru@gmail.com>
2020-04-19 16:17:28 +0900

    If you are building in 32-bit, change it to 64-bit because of an error.

--- native/src/core/Build_at_PyInstaller.bat
+++ native/src/core/Build_at_PyInstaller.bat
@@ -2,7 +2,7 @@
 cd /d %~dp0
 
 
-py -3-32 -m PyInstaller Youtube_dlForExtension.py --onedir --onefile  --clean --noconsole --distpath .\dist_pyinstaller
+py -3-64 -m PyInstaller Youtube_dlForExtension.py --onedir --onefile  --clean --noconsole --distpath .\dist_pyinstaller
 if %ERRORLEVEL% neq 0 (
 pause
 )
